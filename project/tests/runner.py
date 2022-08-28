e0c715471d78de5a3c94e9cbc8529ea6045df062
tschilling <schillingt@better-simple.com>
2022-08-28 09:41:48 -0500

    Automatically exclude lab_test tagged tests.

--- project/tests/runner.py
+++ project/tests/runner.py
@@ -3,6 +3,14 @@ from django.test.utils import override_settings
 
 
 class ProjectTestRunner(DiscoverRunner):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Automatically exclude lab_test tagged tests for normal runs.
+        # This allows a student to run the test suite after completing the lab
+        # without having the lab test error.
+        if "lab_test" not in self.tags:
+            self.exclude_tags.add("lab_test")
+
     def setup_databases(self, **kwargs):
         # Force to always delete the database if it exists
         interactive = self.interactive
