1b4d6aa85ff85bf883a84636749981212a73fe93
tschilling <schillingt@better-simple.com>
2022-09-03 18:56:15 -0500

    Create inclusion tag for rendering an informative post timestamp.

--- project/newsletter/templatetags/newsletter_utils.py
+++ project/newsletter/templatetags/newsletter_utils.py
@@ -1,5 +1,10 @@
+from datetime import timedelta
+
 from django.core.paginator import Paginator
 from django.template import Library
+from django.utils import timezone
+
+from project.newsletter.models import Post
 
 register = Library()
 
@@ -10,3 +15,19 @@ def is_ellipsis(value):
     Determine if the value is an ellipsis
     """
     return value == Paginator.ELLIPSIS
+
+
+@register.inclusion_tag("inclusion_tags/nice_datetime.html")
+def nice_datetime(post: Post, is_unread: bool):
+    """
+    Format the datetime in the locale the user prefers with styling.
+    """
+    now = timezone.now()
+    week_ago = timezone.now() - timedelta(days=7)
+    timestamp = post.publish_date
+    is_recent = week_ago <= timestamp <= now
+    return {
+        "is_unread": is_unread,
+        "is_recent": is_recent,
+        "timestamp": timestamp,
+    }
