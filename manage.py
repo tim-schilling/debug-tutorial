bb8337dfc58951dd5b211cc987f5ee1ee2a19776
tschilling <schillingt@better-simple.com>
2022-09-14 20:26:47 -0500

    Remove coverage from automatically being run.

--- manage.py
+++ manage.py
@@ -7,20 +7,6 @@ import sys
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.config.settings")
 
-    # Project Customization: run coverage.py around tests automatically
-    try:
-        command = sys.argv[1]
-    except IndexError:
-        command = "help"
-
-    running_tests = command == "test"
-    if running_tests:
-        from coverage import Coverage
-
-        cov = Coverage()
-        cov.erase()
-        cov.start()
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -30,11 +16,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-    if running_tests:
-        cov.stop()
-        cov.save()
-        cov.report()
 
 
 if __name__ == "__main__":
