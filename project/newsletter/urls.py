e464c4432203f7cafe8fd248af83504f75b2143c
Tim Schilling <schillingt@better-simple.com>
2026-02-04 17:15:36 -0600

    Remove list and landing views.

--- project/newsletter/urls.py
+++ project/newsletter/urls.py
@@ -5,7 +5,6 @@ from project.newsletter import syndication, views
 app_name = "newsletter"
 urlpatterns = [
     path("markdown/uploader/", views.markdown_uploader, name="markdown_uploader"),
-    path("", views.landing, name="landing"),
     path("account/", views.update_subscription, name="update_subscription"),
     path("analytics/", views.analytics, name="analytics"),
     path("post/unpublished/", views.unpublished_posts, name="unpublished_posts"),
@@ -20,7 +19,6 @@ urlpatterns = [
         "p/",
         include(
             [
-                path("", views.list_posts, name="list_posts"),
                 path("<slug>/", views.view_post, name="view_post"),
             ]
         ),
