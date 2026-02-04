f141e39b1a9d3e285c0f964d5a0b4c0923ac5696
Tim Schilling <schillingt@better-simple.com>
2026-02-04 17:15:15 -0600

    Readd nice_datetime template tag.

--- project/newsletter/test_templatetags.py
+++ project/newsletter/test_templatetags.py
@@ -1,10 +1,22 @@
 from django.core.paginator import Paginator
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.utils import timezone
 
-from project.newsletter.templatetags.newsletter_utils import is_ellipsis
+from project.newsletter.models import Post
+from project.newsletter.templatetags.newsletter_utils import is_ellipsis, nice_datetime
 
 
 class TestIsEllipsis(SimpleTestCase):
     def test_is_ellipsis(self):
         self.assertTrue(is_ellipsis(Paginator.ELLIPSIS))
         self.assertFalse(is_ellipsis("..."))
+
+
+class TestNiceDatetime(TestCase):
+    def test_nice_datetime(self):
+        post = Post(created=timezone.now())
+        actual = nice_datetime(post, is_unread=True)
+
+        self.assertEqual(
+            actual, {"timestamp": post.created, "is_recent": True, "is_unread": True}
+        )
